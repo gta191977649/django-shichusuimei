f2487193f93d88d527cb1e473b1431ac76e704ca
Nurupo <191977649@qq.com>
2024-09-27 01:20:49 +1000

    add

--- frontend/src/components/Element.jsx
+++ frontend/src/components/Element.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 export default function Element(props) {
+  // Define a function to map element names to colors
+  const getElementColor = (element) => {
+    const colorTable = {
+      "forestgreen": ["甲", "乙", "寅", "卯"],
+      "red": ["丙", "丁", "巳", "午"],
+      "saddlebrown": ["戌","戊", "己", "未","丑","辰"],
+      "orange": ["庚", "辛", "申", "酉"],
+      "royalblue": ["壬", "癸", "亥", "子"],
+    };
+    // Loop through each color in the table and check if the element belongs to that color
+    for (const color in colorTable) {
+      if (colorTable[color].includes(element)) {
+        return color;  // Return the color if a match is found
+      }
+    }
+    return "black";  // Return a default color if no match is found
+  };
+
+  // Use the function to get the color for the current element name
+  const color = getElementColor(props.name);
+
+  // Return the JSX with dynamic styling based on the element color
   return (
-    <>
-        <ruby className='text-element'>
-            {props.name}<rt className='vt'>{props.tsuhen}</rt>
-        </ruby>
-    </>
-  )
+    <ruby className='text-element'>
+      <span style={{ color: color, fontWeight: 700 }}>{props.name}</span><rt className='vt'>{props.tsuhen}</rt>
+    </ruby>
+  );
 }
